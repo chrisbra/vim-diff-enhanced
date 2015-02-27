8f0d5ce911b73ea569c1e9bba43c58609d8ed38a
Christian Brabandt <cb@256bit.org>
2015-02-27 13:36:44 +0100

    fix test when running from vundle install

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+# set -ex
 
 for i in */; do
     cd "$i"
     algorithm=$(cat ./diff.txt)
-    vim -u NONE -N --cmd ":let g:enhanced_diff_debug=1" --cmd ":source ~/.vim/plugin/EnhancedDiff.vim" \
-    -c ':set acd' -c ":CustomDiff $algorithm" -c ':botright vsp +next' -c ':windo :diffthis' -c ':qa!' file* \
-    > /dev/null  2>&1
+    ( LC_ALL=C vim -N --cmd ":let g:enhanced_diff_debug=1" -c ':set acd' \
+    -c ":CustomDiff $algorithm" -c ':botright vsp +next' -c ':windo :diffthis' -c ':qa!' file* \
+    > /dev/null ) 2>&1 | sed '/Vim: Warning: Output is not to a terminal/d'
     diff=`diff normal_diff.ok EnhancedDiff_normal.txt`
     if [ $? -ne 0 ]; then
 	printf "Failure with test %s\n" "${i%%/}"
