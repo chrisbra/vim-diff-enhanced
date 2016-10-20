552b47df53211d1502a28a1f9f57e162d6b811e5
Christian Brabandt <cb@256bit.org>
2016-10-20 14:08:44 +0200

    Add test for ignorepat

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,10 @@
 for i in */; do
     cd "$i"
     algorithm=$(cat ./diff.txt)
+    description=''
+    test -f description.txt && description=$(cat ./description.txt)
     ( LC_ALL=C vim -N --cmd ":let g:enhanced_diff_debug=1" -c ':set acd' \
+    -c "if filereadable('vimrc') | so vimrc |endif'" \
     -c ":EnhancedDiff $algorithm" -c ':botright vsp +next' -c ':windo :diffthis' -c ':qa!' file* \
     > /dev/null ) 2>&1 | sed '/Vim: Warning: Output is not to a terminal/d'
     diff=`diff normal_diff.ok EnhancedDiff_normal.txt`
@@ -13,7 +16,11 @@ for i in */; do
 	printf "$diff\n"
 	break
     else
-	printf "Test %s: OK\n" "${i%%/}"
+	if [ -z "$description" ]; then
+	    printf "Test %s: OK\n" "${i%%/}"
+	else
+	    printf "Test %s (%s): OK\n" "${i%%/}" "$description"
+	fi
     fi
     cd ..
 done
